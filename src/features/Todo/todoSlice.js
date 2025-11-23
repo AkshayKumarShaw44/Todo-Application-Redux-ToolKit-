85bda0b7dc7946acf1007b6b85ea7ed49fa64860
Akshay Kumar Shaw <akshaykumarshaw@Akshays-MacBook-Air.local>
2025-11-23 14:11:37 +0530

    Adding LocalStorage

--- src/features/Todo/todoSlice.js
+++ src/features/Todo/todoSlice.js
@@ -1,13 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [
-    {
-      id: nanoid(),
-      todo: "Todo Content",
-      completed: false,
-    },
-  ],
+  todos: JSON.parse(localStorage.getItem("todos")) || []
 };
 
 export const todoSlice = createSlice({
@@ -21,21 +15,27 @@ export const todoSlice = createSlice({
         completed: false,
       };
       state.todos.push(todo);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+
     },
     UpdateTodo: (state, action) => {
       const todo = state.todos.find((t) => t.id === action.payload.id);
       if (todo) {
         todo.todo = action.payload.todo;
       }
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     deleteTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+
     },
     toggleComplete: (state, action) => {
       const todo = state.todos.find((t) => t.id === action.payload.id);
       if (todo) {
         todo.completed = !(todo.completed);
       }
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
   },
 });
